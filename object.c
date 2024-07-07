916243ddb86572f2700928005db838b7b5fc20c0
bababuck <92571492+bababuck@users.noreply.github.com>
2024-07-07 12:28:46 -0700

    Whitepsace

--- object.c
+++ object.c
@@ -3,7 +3,7 @@
 
 /**
  * @brief Private constructor to enforce singleton pattern.
- * 
+ *
  * The constructor is private to prevent direct instantiation of `objects_db_t`.
  * Use `get_objects_db()` to obtain the singleton instance.
  */
@@ -11,10 +11,10 @@ objects_db_t::objects_db_t();
 
 /**
  * @brief Registers an object with a specified name in the database.
- * 
+ *
  * If an object with the same name already exists in the registry, it will be replaced
  * with the new object.
- * 
+ *
  * @param name The name under which to register the object.
  * @param obj Pointer to the object to be registered.
  */
@@ -27,9 +27,9 @@ void objects_db_t::register_object(const name_t &name, const object_t* const obj
 
 /**
  * @brief Looks up and returns the object associated with the specified name.
- * 
+ *
  * If no object is found with the given name, throws a multiple_missing_def_exception_t exception.
- * 
+ *
  * @param name The name of the object to look up.
  * @return Pointer to the object if found, nullptr otherwise.
  */
@@ -45,7 +45,7 @@ object_t* objects_db_t::lookup_object(const name_t &name) const {
  * @brief Retrieves the singleton instance of `objects_db_t`.
  * 
  * If the instance does not yet exist, it will be created on the first call.
- * 
+ *
  * @return Pointer to the singleton instance of `objects_db_t`.
  */
 static objects_db_t* objects_db_t::get_objects_db(){
@@ -57,7 +57,7 @@ static objects_db_t* objects_db_t::get_objects_db(){
 
 /**
  * @brief Constructor to initialize an object with a specified name.
- * 
+ *
  * @param _name The name to be associated with the object.
  */
 object_t::object_t(name_t _name) : name(_name) {}
--- object.h
+++ object.h
@@ -5,11 +5,11 @@ typedef std::string name_t;
 
 /**
  * @brief Singleton class using the Registry design pattern to manage a database of objects.
- * 
+ *
  * The Registry design pattern is used to maintain a central registry of objects, allowing
  * efficient lookup and management. This class provides methods to register objects by name
  * and retrieve them later using their registered names.
- * 
+ *
  * The singleton pattern ensures that only one instance of `objects_db_t` exists throughout
  * the program's lifetime, accessible globally via the `get_objects_db()` static method.
  */
@@ -27,7 +27,7 @@ public:
 
 /**
  * @brief Base class for all objects in the program.
- * 
+ *
  * This class serves as the fundamental building block for all other types in the program.
  * It encapsulates a name associated with each object and provides a constructor to initialize
  * this name upon object creation.
