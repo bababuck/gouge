690315b3251814d24b09c42a56cbf51ea1e0ed86
bababuck <92571492+bababuck@users.noreply.github.com>
2024-07-07 17:22:44 -0700

    Split header

--- function.h
+++ function.h
@@ -5,12 +5,20 @@
 #ifndef FUNCTION_H
 #define FUNCTION_H
 
-void* new_function(char *name, void **outputs, void **inputs);
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void* new_function(char *name, void *outputs, void *inputs);
 
 void evaluate_function_equations(void *function, void *equations);
 
 void* add_equation(void *old_equations, void *equation);
 
 void* make_equation(char *lhs_wire, char op, char *rhs_wire);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
--- types.h
+++ types.h
@@ -5,8 +5,16 @@
 #ifndef TYPES_H
 #define TYPES_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void* add_wires(void *old_wires, void *wire);
 
 void* new_wire(char *type, char *name);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
