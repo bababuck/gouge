00c5958705f903895ff5fe67d21c9d7930b368e5
bababuck <92571492+bababuck@users.noreply.github.com>
2024-07-11 00:44:33 -0700

    Add general_exception_t and make multiple_missing_def_exception_t inherit from it
    All exceptions should inherit from general_exception_t for this program.

--- exceptions.cpp
+++ exceptions.cpp
@@ -8,12 +8,18 @@
  * @brief Constructor for initializing the exception with a message.
  * @param msg The error message to be associated with the exception.
  */
-multiple_missing_def_exception_t::multiple_missing_def_exception_t(const std::string& msg) : message(msg) {}
+general_exception_t::general_exception_t(const std::string& msg) : message(msg) {}
 
 /**
  * @brief Returns the error message associated with the exception.
  * @return A C-string pointer to the error message.
  */
-const char* multiple_missing_def_exception_t::what() const noexcept {
+const char* general_exception_t::what() const noexcept {
     return message.c_str();
 }
+
+/**
+ * @brief Constructor for initializing the exception with a message.
+ * @param msg The error message to be associated with the exception.
+ */
+multiple_missing_def_exception_t::multiple_missing_def_exception_t(const std::string& msg) : general_exception_t(msg) {}
--- exceptions.hpp
+++ exceptions.hpp
@@ -9,19 +9,26 @@
 #include <string>
 
 /**
- * @brief Exception class for indicating multiple missing definitions.
+ * @brief Exception class for indicating micellaneous exceptions.
  *
  * This class inherits from std::exception and provides a custom message
  * for describing the exception.
  */
-class multiple_missing_def_exception_t : public std::exception {
+class general_exception_t : public std::exception {
 private:
     std::string message;
 
 public:
-    multiple_missing_def_exception_t(const std::string& msg);
+    general_exception_t(const std::string& msg);
 
     virtual const char* what() const noexcept override;
 };
 
+/**
+ * @brief Exception class for indicating multiple missing definitions.
+ */
+class multiple_missing_def_exception_t : public general_exception_t {
+public:
+    multiple_missing_def_exception_t(const std::string& msg);
+};
 #endif
