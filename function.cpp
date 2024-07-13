c3bdc596f3f8075efb476824e0f0a59e35afb49e
bababuck <92571492+bababuck@users.noreply.github.com>
2024-07-13 11:01:28 -0700

    Cleanup function.cpp

--- function.cpp
+++ function.cpp
@@ -10,15 +10,15 @@ extern "C"
 {
 /**
  * @brief Creates a new function object.
- * 
+ *
  * This function creates a new function object using the provided name,
  * outputs, and inputs. It initializes the function with the specified
  * wires for outputs and inputs.
- * 
+ *
  * @param name Pointer to a C-string representing the name of the function.
  * @param outputs Pointer to an array of pointers to output wires.
  * @param inputs Pointer to an array of pointers to input wires.
- * 
+ *
  * @return Pointer to the created function object (function_t* case to void*).
  */
 void* new_function(char *name, void *outputs, void *inputs) {
@@ -37,12 +37,12 @@ void evaluate_function_equations(void *function, void *equations) {
 
 /**
  * @brief Adds a wire to the collection.
- * 
+ *
  * @param wire Pointer to the wire object to add.
- * 
+ *
  * @todo Condense this down to use data from objects_db_t.
- * 
- * @note This method adds a wire object to the collection. 
+ *
+ * @note This method adds a wire object to the collection.
  * It associates the wire with its name in the internal map.
  */
 void wire_collection_t::add(wire_t *wire) {
@@ -51,11 +51,11 @@ void wire_collection_t::add(wire_t *wire) {
 
 /**
  * @brief Constructor to initialize a function object.
- * 
+ *
  * @param _name Name of the function.
  * @param outputs Pointer to a list of output wires.
  * @param inputs Pointer to a list of input wires.
- * 
+ *
  * Constructs a function object with given name, and adds the specified
  * output and input wires to respective collections.
  */
@@ -81,15 +81,15 @@ extern "C"
 {
 /**
  * @brief Adds an equation to the list of equations.
- * 
+ *
  * This function adds a new equation to an existing list of equations or creates
  * a new list if none exists. The equation is appended to the end of the list.
- * 
+ *
  * @param old_equations Pointer to the old list of equations.
  * @param equation Pointer to the equation to add.
- * 
+ *
  * @return Pointer to the updated list of equations (std::vector<equation_t*>*).
- * 
+ *
  * @note If `old_equations` is `nullptr`, a new std::vector<equation_t*> is allocated.
  */
 void* add_equation(void *old_equations, void *equation) {
@@ -103,17 +103,17 @@ void* add_equation(void *old_equations, void *equation) {
 
 /**
  * @brief Creates an equation using given operands and operator.
- * 
+ *
  * This function creates an equation using the provided left-hand side wire,
  * operator, and right-hand side wire. It constructs equation objects and
  * returns a pointer to the resulting equation.
- * 
+ *
  * @param lhs_wire Pointer to a C-string representing the left-hand side wire name.
  * @param op Character representing the operator to use ('+', '-', '*', etc.).
  * @param rhs_wire Pointer to a C-string representing the right-hand side wire name.
- * 
+ *
  * @return Pointer to the created equation object (equation_t*).
- * 
+ *
  * @note The returned equation represents lhs_wire op rhs_wire.
  */
 void* make_equation(char *lhs_wire, char _op, char *rhs_wire) {
@@ -126,9 +126,9 @@ void* make_equation(char *lhs_wire, char _op, char *rhs_wire) {
 
 /**
  * @brief Constructor to initialize an operator object.
- * 
+ *
  * @param _operator The character representing the operator ('+', '-', '*', etc.).
- * 
+ *
  * Initializes the operator with the given character.
  */
 operator_t::operator_t(char _op): op(_op) {}
